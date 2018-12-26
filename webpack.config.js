982405211b31549d10c4ef12f845e121a7fd3656
Manuel Torrez <manuel.torrez.vado@protonmail.com>
2018-12-26 09:04:33 -0600

    Configuring watch mode for webpack. Fuck webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    watch: true,
     entry: './index.js',
     output: {
         filename: 'bundle.js',
